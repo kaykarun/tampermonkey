4d9c4e314a8ea228d85bd94643324982bb195617
kaykarun <8163578+kaykarun@users.noreply.github.com>
2021-02-15 19:47:27 -0800

    Add files via upload

---
+++ discord.user.js
@@ -0,0 +1,232 @@
+// ==UserScript==
+// @name         discord_script
+// @namespace    familysicle
+// @version      0.5
+// @description  try to take over the world!
+// @author       You
+// @match        https://discord.com/*
+// @grant        window.onurlchange
+// @grant        GM_setClipboard
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @noframes
+// @updateURL    https://drive.google.com/file/d/1qZLwi9mKV500P6Wr0xI-wPKnjpUj9-uO/view
+// @downloadURL  https://drive.google.com/file/d/1qZLwi9mKV500P6Wr0xI-wPKnjpUj9-uO/view
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+debugger;
+
+var timemap_default = {
+    "date" : 0,
+    "familysicle" : 0,
+    "testserver" : 0,
+    "WRRF CADathon Discord": 0
+};
+
+var servers = [
+    {
+        "name" : "familysicle",
+        "id" : "809905984209551390",
+        "url" : "https://discord.com/channels/809905984209551390/809905984209551393",
+        "hide" : ["none"]
+    },
+    {
+        "name" : "testserver",
+        "id" : "809908088185684049",
+        "url" : "https://discord.com/channels/809908088185684049/809908088697913346",
+        "hide" : ["lightsout", "school"]
+    },
+    {
+        "name" : "WRRF CADathon Discord",
+        "id" : "749773878162620616",
+        "url" : "https://discord.com/channels/749773878162620616/749791320972722196",
+        "hide" : ["lightsout", "school", "!breaks"]
+    }
+];
+
+var timeblocks = {
+    "lightsout" : [
+        [0, 6], [0, 6], [0, 60],
+        [0, 6], [22, 23], [0, 60]
+    ],
+    "school" : [
+        [1, 1], [7, 7], [30, 60],
+        [1, 1], [8, 13], [0, 60],
+        [2, 2], [8, 8], [30, 60],
+        [2, 2], [9, 14], [0, 60],
+        [2, 2], [15, 15], [0, 60],
+        [3, 3], [9, 10], [0, 60],
+        [4, 4], [8, 13], [0, 60],
+        [4, 4], [8, 13], [0, 60],
+        [5, 5], [8, 8], [30, 60],
+        [5, 5], [9, 14], [0, 60],
+        [5, 5], [15, 15], [0, 60]
+    ],
+    "breaks" : [
+        [1, 1], [9, 9], [30, 45],
+        [1, 1], [11, 11], [15, 60],
+        [1, 1], [12, 12], [0, 15],
+        [2, 2], [10, 10], [30, 45],
+        [2, 2], [12, 12], [15, 60],
+        [2, 2], [13, 13], [0, 60],
+        [4, 4], [9, 9], [30, 45],
+        [4, 4], [11, 11], [15, 60],
+        [4, 4], [12, 12], [0, 15],
+        [5, 5], [10, 10], [30, 45],
+        [5, 5], [12, 12], [15, 60],
+        [5, 5], [13, 13], [0, 60]
+    ]
+}
+
+
+var familysicleURL = "https://discord.com/channels/809905984209551390/809905984209551393";
+
+function matchTimeBlock(key) {
+    var timeblock = timeblocks[key];
+    var i, d = new Date();
+    var dy = d.getDay();
+    var hr = d.getHours();
+    var mn = d.getMinutes();
+    var cnt = timeblock.length / 3;
+    for (i = 0; i < cnt; i++) {
+        if (dy >= timeblock[i*3][0] && dy <= timeblock[i*3][1]) {
+            if (hr >= timeblock[i*3+1][0] && hr <= timeblock[i*3+1][1]) {
+                if (mn >= timeblock[i*3+2][0] && mn <= timeblock[i*3+2][1]) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function listChannels() {
+    var i, channels = "", href;
+    $( "div[href*='channels']" ).each(function(i, obj) {
+        href = obj.attributes.href.nodeValue;
+        if (href.indexOf("@me") == -1) {
+            var newurl = true;
+            for (i=0; i < servers.length; i++) {
+                if (servers[i].url.indexOf(href) != -1) {
+                    newurl = false;
+                }
+            }
+            if (newurl) {
+                channels = channels + href + ":" + obj.attributes["aria-label"].nodeValue + "\n";
+            }
+        }
+    });
+    if (channels != "") {
+        GM_setClipboard(channels);
+        alert("New channels list copied to clipboard!");
+    }
+
+    $( ".listItem-2P_4kh" ).each(function(i, obj) {
+        var j;
+        for (j=0; j < servers.length; j++) {
+            var s = servers[j];
+            if(obj.innerHTML.indexOf(s.name) != -1) {
+                obj.id = s.id;
+            }
+        }
+    });
+
+}
+
+function justWait() {
+    var guilds = document.getElementsByClassName("listItem-2P_4kh");
+    if (guilds.length>6) {
+        listChannels();
+        hideServers();
+    } else {
+        setTimeout(function(){
+            justWait()
+        }, 2000);
+    }
+}
+
+function shouldHide(blocks) {
+    var i;
+    var hide = false, key;
+    for (i=0; i<blocks.length; i++) {
+        if (blocks[i] == "none") {
+            return false;
+        }
+        key = blocks[i];
+        if (key.substring(0, 1) != "!") {
+            if (matchTimeBlock(key)) {
+                hide = true;
+            }
+        } else {
+            key = key.substring(1);
+            if (matchTimeBlock(key)) {
+                hide = false;
+            }
+        }
+    }
+    return hide;
+}
+
+function hideServers() {
+    var j;
+    for (j=0; j < servers.length; j++) {
+        var s = servers[j];
+        if (shouldHide(s.hide)) {
+            $( "#"+s.id ).hide();
+        } else {
+            $( "#"+s.id ).show();
+        }
+    }
+}
+
+function switchToFamily(newurl) {
+    var j;
+    for (j=0; j< servers.length; j++) {
+        var s = servers[j];
+        if (shouldHide(s.hide) && newurl.href.indexOf(s.id) != -1) {
+            window.location = familysicleURL;
+        }
+    }
+}
+
+function trackTime() {
+    if (document.visibilityState != "visible") {
+        return;
+    }
+    var d = new Date();
+    var dt = d.getDate();
+    var timemap = GM_getValue("timemap", timemap_default);
+    if (timemap.date == undefined || timemap.date != dt) {
+        timemap = timemap_default;
+        timemap.date = dt;
+    }
+    var i;
+    for (i=0; i < servers.length; i++) {
+        var s = servers[i];
+        if (window.location.href == s.url) {
+            timemap[s.name] = timemap[s.name] + 1
+        }
+    }
+    GM_setValue("timemap", timemap);
+    $( ".name-1jkAdW" ).each(function(i, obj) {
+        var j, nm;
+        var idx = obj.innerHTML.indexOf(":");
+        if(idx != -1) {
+            nm = obj.innerHTML.substring(idx+1);
+        } else {
+            nm = obj.innerHTML;
+        }
+        obj.innerHTML = Math.floor(timemap[nm]/6) + ":" + nm;
+    });
+}
+
+if (window.onurlchange === null) {
+    window.addEventListener('urlchange', (event) => switchToFamily(event.url));
+}
+
+switchToFamily(window.location);
+justWait();
+
+setInterval(hideServers, 60000);
+setInterval(trackTime, 10000);
---
+++ timetracker.user..js
@@ -0,0 +1,233 @@
+// ==UserScript==
+// @name         Time Tracking Helper
+// @namespace    familysicle
+// @version      0.6
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @match        http://192.168.55.1/*
+// @grant        window.onurlchange
+// @grant        GM_setClipboard
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_openInTab
+// @noframes
+// @updateURL    https://drive.google.com/file/d/19H0TB0LMWEDfDDSqIjcgjuf4ODQ50sV3/view
+// @downloadURL  https://drive.google.com/file/d/19H0TB0LMWEDfDDSqIjcgjuf4ODQ50sV3/view
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+var reportURL = "http://192.168.55.1/report";
+var topN = 10;
+
+function getStorageName() {
+    var d = new Date();
+    var m = d.getMonth();
+    var dt = d.getDate();
+    if (m > 1 || dt > 15) {
+        return "timemap_"+dt;
+    }
+    return "timemap";
+}
+
+function mapPathNames() {
+    if (window.location.hostname.indexOf("discord.com") != -1) {
+        return mapDiscordURLs();
+    }
+    if (window.location.hostname.indexOf("www.youtube.com") != -1 &&
+        window.location.pathname.indexOf("/watch") != -1) {
+        return mapYouTubeURLs();
+    }
+    return window.location.pathname;
+}
+
+function mapYouTubeURLs() {
+    var title = document.querySelector('meta[name="title"]').content;
+    if (title == undefined) {
+        title = document.querySelector('title').innerText;
+    }
+    return title.substr(0, 60);
+}
+
+function mapDiscordURLs() {
+    var nm;
+    if (window.location.pathname.indexOf('@me') == -1) {
+        nm = $( '.name-1jkAdW' );
+        //console.log("Channel "+nm[0].innerText);
+        nm = nm[0].innerText;
+        if (nm.indexOf(":") != -1) {
+            nm = nm.substring(nm.indexOf(":")+1);
+        }
+        return nm;
+    } else if (window.location.pathname.indexOf('/@me/') != -1){
+        nm = $( '.selected-aXhQR6' );
+        //console.log("DM "+nm[0].innerText);
+        return "DM: "+nm[0].innerText;
+    }
+    return window.location.pathname;
+}
+
+function trackTime() {
+    if (window.location.href.indexOf(reportURL) != -1) {
+        return;
+    }
+    if (document.visibilityState != "visible") {
+        return;
+    }
+    // Skip iframes
+    if (window.top != window.self) {
+        return;
+    }
+
+    var d = new Date();
+    var dt = d.getDate();
+    var timemap = GM_getValue(getStorageName(), {});
+    if (timemap.date == undefined || timemap.date != dt) {
+        timemap = {};
+        timemap.date = dt;
+    }
+    var hostname = window.location.hostname;
+    var pathname = mapPathNames();
+    if (timemap[hostname] == undefined) {
+        timemap[hostname] = { "total" : 0 };
+    }
+    var total = timemap[hostname].total;
+    var pathtime = timemap[hostname][window.location.pathname] || timemap[hostname][pathname] || 0;
+    timemap[hostname].total = total + 1;
+    timemap[hostname][pathname] = pathtime + 1;
+    if (window.location.pathname != pathname) {
+        delete timemap[hostname][window.location.pathname];
+    }
+    GM_setValue(getStorageName(), timemap);
+}
+
+var chart = {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Top Sites Visited"
+	},
+	axisY: {
+		title: "Time (mins)"
+	},
+	data: [{
+        //click: chartDrillDownHandler,
+		type: "bar",
+		showInLegend: true,
+		legendMarkerColor: "grey",
+		dataPoints: []
+	}]
+};
+
+function getTimeData(dataPoints, domain) {
+    var timemap = GM_getValue(getStorageName(), {});
+    var tuples = [];
+    if (domain == "") {
+        for (var key in timemap) {
+            if (key != "date") {
+                tuples.push([key, timemap[key].total/4]);
+            }
+        }
+    } else {
+        var map = timemap[domain];
+        for (key in map) {
+            if (key != "total") {
+                tuples.push([key, map[key]/4]);
+            }
+        }
+    }
+    tuples.sort(function(a, b) {
+        a = a[1];
+        b = b[1];
+
+        return a < b ? -1 : (a > b ? 1 : 0);
+    });
+    var i, k, v = 0, bottomN = 0;
+    if (tuples.length > topN) {
+        bottomN = tuples.length - topN;
+    }
+    for (i = 0; i < bottomN; i++) {
+        v += tuples[i][1];
+    }
+    if (v > 0) {
+        dataPoints.push({"label": "other sites", "y": v});
+    }
+
+    for (i = bottomN; i < tuples.length; i++) {
+        k = tuples[i][0];
+        v = tuples[i][1];
+        dataPoints.push({"label": k, "y": v});
+    }
+}
+
+function renderChart() {
+    if (window.location.href.indexOf(reportURL) == -1) {
+        return;
+    }
+
+    var domain = window.location.href.substring(reportURL.length+1);
+    if (domain == "other%20sites") {
+        domain = "";
+    }
+
+    while (document.head.firstChild) {
+        document.head.removeChild(document.head.firstChild);
+    }
+    while (document.body.firstChild) {
+        document.body.removeChild(document.body.firstChild);
+    }
+    document.title = "Time tracker report";
+
+    //<meta http-equiv="refresh" content="30">
+    var elem = document.createElement("meta");
+    elem.setAttribute("http-equiv", "refresh");
+    elem.setAttribute("content", "30");
+    document.head.appendChild(elem);
+
+    getTimeData(chart.data[0].dataPoints, domain);
+
+    var script = document.createElement("script");
+    script.type = 'text/javascript';
+    script.innerText = "";
+    script.innerText = script.innerText + "function chartDrilldownHandler(e) { ";
+    //script.innerText = script.innerText + "console.log(e.dataPoint.label);";
+    script.innerText = script.innerText + "window.location.href = '";
+    if (domain == "") {
+        script.innerText = script.innerText + reportURL + "/'+e.dataPoint.label;";
+    } else {
+        script.innerText = script.innerText + reportURL + "';";
+    }
+    script.innerText = script.innerText + "} \n";
+    script.innerText = script.innerText + "function showChart() { var chart =  new CanvasJS.Chart('chartContainer',";
+    script.innerText = script.innerText + JSON.stringify(chart);
+    script.innerText = script.innerText + "); \n";
+    script.innerText = script.innerText + "chart.options.data[0].click = chartDrilldownHandler; \n";
+    if (domain != "") {
+        script.innerText = script.innerText + "chart.options.title.text = '"+ domain +"'; \n";
+    }
+    script.innerText = script.innerText + "chart.render();} \n";
+    document.head.appendChild(script);
+
+    script = document.createElement("script");
+    script.type = 'text/javascript';
+    script.setAttribute("src", "https://canvasjs.com/assets/script/canvasjs.min.js");
+    script.setAttribute("onload", "showChart()");
+    document.head.appendChild(script);
+
+    var div = document.createElement('div');
+    div.id = 'chartContainer';
+    div.style.cssText = 'height: 600px; width: 80%; margin: auto;';
+    document.body.appendChild(div);
+}
+
+function openReport() {
+    GM_openInTab(reportURL, {'active': true});
+}
+
+if (window.location.href != reportURL) {
+    GM_registerMenuCommand("Show Report", openReport);
+}
+
+renderChart();
+setInterval(trackTime, 15000);
